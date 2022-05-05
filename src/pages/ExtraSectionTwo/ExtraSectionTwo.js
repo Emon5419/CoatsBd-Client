9f9a0c7a53ecd76c14bd8fe987fc04d36a9742e1
Mohammad Alauddin Emon <95302046+Emon5419@users.noreply.github.com>
2022-05-05 23:10:24 +0600

    ExtraSectionTwo init

---
+++ src/pages/ExtraSectionTwo/ExtraSectionTwo.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './ExtraSectionTwo.css';
+import { Card, CardGroup } from 'react-bootstrap';
+import name1 from '../../images/Gosnell.jpg';
+import name2 from '../../images/Rajiv_Sharma.png';
+import name3 from '../../images/Nicholas_Bull.jpg';
+const ExtraSectionTwo = () => {
+   return (
+      <div className='mt-5'>
+         <h5 className='text-center underline'><u>Board of Directors</u></h5>
+         <CardGroup className='contanier'>
+
+            <Card>
+               <Card.Img variant="top" src={name1} />
+               <Card.Body>
+                  <Card.Title>David Gosnell OBE</Card.Title>
+                  <Card.Text>
+                     Chairman
+                  </Card.Text>
+               </Card.Body>
+            </Card>
+
+            <Card>
+               <Card.Img variant="top" src={name2} />
+               <Card.Body>
+                  <Card.Title>Rajib Sharma</Card.Title>
+                  <Card.Text>
+                     Group Chief Executive
+                  </Card.Text>
+               </Card.Body>
+            </Card>
+
+            <Card>
+               <Card.Img variant="top" src={name3} />
+               <Card.Body>
+                  <Card.Title>Nichoals Bull</Card.Title>
+                  <Card.Text>
+                     Senior Independent Non-Executive Director
+                  </Card.Text>
+               </Card.Body>
+            </Card>
+
+         </CardGroup>
+      </div>
+   );
+};
+
+export default ExtraSectionTwo;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ExtraSectionOne from '../ExtraSectionOne/ExtraSectionOne';
+import ExtraSectionTwo from '../ExtraSectionTwo/ExtraSectionTwo';
 import Product from '../Product/Product';
 import Banner from '../shared/Banner/Banner';
 import Footer from '../shared/Footer/Footer';
@@ -9,6 +10,7 @@ const Home = () => {
       <div>
          <Banner></Banner>
          <ExtraSectionOne></ExtraSectionOne>
+         <ExtraSectionTwo />
          <Product></Product>
          <Footer></Footer>
       </div>
