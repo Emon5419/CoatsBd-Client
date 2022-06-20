47f5b6d8d4348001df5c8d6946131d9b26f86912
Mohammad Alauddin Emon <95302046+Emon5419@users.noreply.github.com>
2022-06-20 22:52:35 +0600

    deliver btn & restock field init

--- src/pages/Home/ProductDetail/ProductDetail.js
+++ src/pages/Home/ProductDetail/ProductDetail.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './ProductDetail.css';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import useProducts from '../../../hooks/useProducts/useProducts';
 
 const ProductDetail = () => {
+   const [quantity, setQuantity] = useState(100);
 
    const { productId } = useParams();
-   const [products, setProducts] = useProducts();
 
    const [product, setProduct] = useState({});
 
@@ -20,36 +20,38 @@ const ProductDetail = () => {
    });
 
 
-   const handleDeliverItems = id => {
-      const proceed = window.confirm('Delete This items?')
-      if (proceed) {
-
-         const url = `http://localhost:5000/product/${id}`;
-         fetch(url, {
-            method: 'DELETE'
-         })
-            .then(res => res.json())
-            .then(data => {
-               console.log(data);
-               const remaining = products.filter(product => product._id !== id)
-               setProducts(remaining);
-            });
-      }
-   }
    return (
+      <div>
+         <div className='d-flex detail-container w-50 mx-auto mt-5'>
+            <div>
+               <img className='w-75' src={product.img} alt="" />
+            </div>
+            <div>
+               <h5>Name: {product.name}</h5>
+               <h6>Quantity: {quantity}</h6>
+               <h6>Price: {product.price}</h6>
+               <h6>Brand: {product.brand}</h6>
+               <h6>Supplier: {product.supplier}</h6>
+               <h6>Description: {product.des}</h6>
 
-      <div className='d-flex detail-container w-50 mx-auto mt-5'>
-         <div>
-            <img className='w-75' src={product.img} alt="" />
+               <Button className='w-100' onClick={
+                  () => {
+                     setQuantity(quantity - 1);
+                  }
+               }>Deliver</Button>
+            </div>
          </div>
          <div>
-            <h5>Name: {product.name}</h5>
-            <h6>Quantity: 500Kg</h6>
-            <h6>Price: {product.price}</h6>
-            <h6>Brand: {product.brand}</h6>
-            <h6>Supplier: {product.supplier}</h6>
-            <h6>Description: {product.des}</h6>
-            <Button className='w-100' onClick={() => handleDeliverItems(product._id)}>Deliver</Button>
+            <Form className='text-center mt-5'>
+               <Form.Label><u>Add Quantity Here</u></Form.Label>
+               <Form.Group className="mb-3 w-50 mx-auto">
+
+                  <Form.Control onChange={
+                     (e) => setQuantity(quantity + parseInt(e.target.value))} type="number" placeholder="Enter Quantity" />
+
+               </Form.Group>
+            </Form>
+
          </div>
       </div>
    );
